d37c5b2c96554b136fe29e2724efed6955ee3f75
viviane <v.buschwallace@yahoo.com>
2021-06-08 13:23:32 +0200

    created return of App

--- src/App.js
+++ src/App.js
@@ -2,7 +2,19 @@ import "./components/Loading";
 import "./components/CountriesOutput";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <form>
+        <input
+          type="text"
+          value={userInput}
+          placeholder="Type a country name"
+        />
+        <button type="submit">Search</button>
+      </form>
+      <CountriesOutput className="results" />
+    </div>
+  );
 }
 
 export default App;
