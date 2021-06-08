2c7053af9ddd7c390b2c489fb751d0388e04d943
viviane <v.buschwallace@yahoo.com>
2021-06-08 15:40:29 +0200

    *hits head on desk* where is my mistake....

--- src/components/CountriesOutput.js
+++ src/components/CountriesOutput.js
@@ -1,18 +1,36 @@
 import React from "react";
 
-function ShowResults({ results }) {
-  const countryInfo = results.map((info) => {
-    console.log(info);
-  });
-  return (
-    <div className="search-results">
-      <div>{results}</div>
-      {/* <div className="flag-wrapper">
-        <img src={flag} alt={name} className="flag-image" />
-      </div>
-      <h2 className="country-name">{name}</h2> */}
-    </div>
-  );
-}
+// function ShowResults({ results }) {
+//   const countryInfo = results.map((obj) => {
+//     console.log(obj);
+//     return <div>{results}</div>;
+//   });
+//   return (
+//     <div className="search-results" key={something}>
+//       {/* <div className="flag-wrapper">
+//         <img src={flag} alt={name} className="flag-image" />
+//       </div>
+//       <h2 className="country-name">{name}</h2> */}
+//     </div>
+//   );
+// }
 
-export default ShowResults;
+// export default ShowResults;
+
+const Country = ({ results }) => {
+  const lands = results.map((obj, i) => {
+    console.log(obj);
+    const { name, flag, capital, population } = obj;
+    return (
+      <div key={i}>
+        <h2>{name}</h2>
+        <h5>Capital: {capital}</h5>
+        <h5>Population: {population}</h5>
+        <img src={flag} alt={name} className="flag" />
+      </div>
+    );
+  });
+
+  return <React.Fragment>{lands}</React.Fragment>;
+};
+export default Country;
