935040dfeb7a9c31ce0cc8088e532b19edf732b7
viviane <v.buschwallace@yahoo.com>
2021-06-08 13:32:34 +0200

    added handleSubmit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React, { useState, useEffect } from "react";
+// import axios, the helper for promises
 import axios from "axios";
 
+// import components
 import "./components/Loading";
 import "./components/CountriesOutput";
 
+// what happens on clicking "Search"
 function App() {
+  function handleSubmit(e) {
+    // prevent default of form
+    e.preventDefault();
+  }
+
   return (
     <div className="App">
-      <form>
+      <form onSubmit={handleSubmit}>
         <input
           type="text"
           value={userInput}
