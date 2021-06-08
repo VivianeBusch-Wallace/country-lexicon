fdbe448e55042f49190945269120a725009b2051
viviane <v.buschwallace@yahoo.com>
2021-06-08 16:23:47 +0200

    added languages

--- src/components/CountriesOutput.js
+++ src/components/CountriesOutput.js
@@ -3,7 +3,9 @@ import React from "react";
 function ShowResults({ results }) {
   const countryInfo = results.map((info, i) => {
     console.log(info);
-    const { name, flag, capital, population } = info;
+    const { name, flag, capital, languages } = info;
+    console.log(languages[0][name]);
+    const lang = languages.map((language) => language.name);
     return (
       <div className="search-results" key={i}>
         <div className="flag-wrapper">
@@ -13,6 +15,7 @@ function ShowResults({ results }) {
         <p>
           {name}'s capital is: {capital}
         </p>
+        <p>Languages: {lang}</p>
       </div>
     );
   });
